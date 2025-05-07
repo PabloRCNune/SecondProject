d4132fc1f23cb04a68485cafbfea22442761989d
Pablo Cascón <pablo.rodriguez@nunegal.com>
2025-05-07 16:30:03 +0200

    [ADD] enlace modificar y segunda tabla de los botones

--- src/main/java/com/example/PortalEmpleados/controller/EmpleadoController.java
+++ src/main/java/com/example/PortalEmpleados/controller/EmpleadoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.PortalEmpleados.entity.Empleado;
 import com.example.PortalEmpleados.repo.EmpleadoRepo;
@@ -23,8 +24,8 @@ public class EmpleadoController {
 	}
 
 	@GetMapping("/new/empleado")
-	public String newEmpleado(Model model) {
-		Empleado empleado = new Empleado();
+	public String newEmpleado(Model model, @RequestParam(name = "id",required = false, defaultValue = "-1") long empleadoId) {
+		Empleado empleado = empleadoRepo.findById(empleadoId).orElse(new Empleado());
 		model.addAttribute("empleado", empleado);
 		return "newEmpleado";
 	}
@@ -36,10 +37,33 @@ public class EmpleadoController {
 	}
 
 	@PostMapping("/save")
-	public String saveEmpleado(@ModelAttribute Empleado empleado) {
+	public String saveEmpleado(@ModelAttribute Empleado empleado, @RequestParam long id) {
+		empleado.setId(id);
+		System.out.println("Empleado: " + empleado);
 		empleadoRepo.save(empleado);
 		return "redirect:/lista";
 
+	}
+	@GetMapping("/new/empleado2")
+	public String newEmpleadoButtons(Model model, @RequestParam(name = "id",required = false, defaultValue = "-1") long empleadoId) {
+		Empleado empleado = empleadoRepo.findById(empleadoId).orElse(new Empleado());
+		model.addAttribute("empleado", empleado);
+		return "newEmpleado";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String removeEmpleadoButtons(@PathVariable long id) {
+		empleadoRepo.deleteById(id);
+		return "redirect:/lista";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmpleadoButtons(@ModelAttribute Empleado empleado, @RequestParam long id) {
+		empleado.setId(id);
+		System.out.println("Empleado: " + empleado);
+		empleadoRepo.save(empleado);
+		return "redirect:/lista";
+		
 	}
 
 }
